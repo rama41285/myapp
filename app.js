22f7ca6a5381f4a138760e3d90700395b4299290
rama41285 <rama4128577@gmail.com>
2018-04-21 14:18:45 +0530

    make it better

---
+++ app.js
@@ -0,0 +1,6 @@
+console.log('Hello World .. this is the first node js application')
+function greetNewUser (userName) {
+  const uName = userName
+  console.log('Greeting new user successfully: ' + uName)
+}
+greetNewUser('ramalinr')
